0b0b2732e6b0bd4a09c4d40405b2265d678f921f
Bastian Ermann <bastian@btie.cl>
2017-08-31 12:39:38 -0300

    fix: Use React Router to handle navigation.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks onNavigate={ () => {
-            this.setState({
-              showSearchPage: false
-            })
-          }}/>
-        ) : (
-          <ListBooks onNavigate={ () => {
-            this.setState({
-              showSearchPage: true
-            })
-          }}/>
-        )}
+        <Route
+          path='/search'
+          component={SearchBooks}
+        />
+        <Route exact
+          path='/'
+          component={ListBooks}
+        />
       </div>
     )
   }
