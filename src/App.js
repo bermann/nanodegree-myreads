6fe9ae50aa6b27aaeb87487e7b1d8f2c69f3d706
Bastian Ermann <bastian@btie.cl>
2017-08-31 13:20:05 -0300

    feat: Get books from API
    The books are retrieved from the backend using BooksAPI.
    
    The books are not shown in its appropriate shelf, all the books are
    shown as they where in a "All Books" shelf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) =>{
+      this.setState({
+        books: books
+      })
+    })
+  }
+
   render() {
+    const { books } = this.state
+
     return (
       <div className="app">
         <Route
@@ -15,7 +30,10 @@ class BooksApp extends React.Component {
         />
         <Route exact
           path='/'
-          component={ListBooks}
+          render={ () => (
+            <ListBooks
+              books={books}/>
+          )}
         />
       </div>
     )
--- src/Book.js
+++ src/Book.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 class Book extends Component {
   render() {
     const { book } = this.props
-    console.log(book);
 
     return (
       <div className="book">
